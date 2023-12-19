4129898021e1dc6e87c51f1b52b8e17a5404dce0
Melocotron <uriarcia@gmail.com>
2023-12-19 17:37:27 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Firmas JC</h1>
+    <img src="./public/Angel-Arcia.jpg" alt="" style="max-width: 300px" />
+    <img src="./public/Nallely-Moontes.jpg" alt="" style="max-width: 300px"/>
+  </div>
+`;
+|
