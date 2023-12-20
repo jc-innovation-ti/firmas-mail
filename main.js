aef0eef5234bcc8f8b4581b54c5742567d2d0e68
Angel Arcia <aarcia@jc-innovation.com>
2023-12-20 09:31:54 -0600

    feat: path  images

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import "./style.css";
 document.querySelector("#app").innerHTML = `
   <div>
     <h1>Firmas JC</h1>
-    <img src="./public/Angel-Arcia.jpg" alt="" style="max-width: 300px" />
-    <img src="./public/Nallely-Moontes.jpg" alt="" style="max-width: 300px"/>
+    <img src="images/Angel-Arcia.jpg" alt="" style="max-width: 300px" />
+    <img src="images/Nallely-Moontes.jpg" alt="" style="max-width: 300px"/>
   </div>
 `;
